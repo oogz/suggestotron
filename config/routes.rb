3210c32c368df4a46ae3733453d456ff76484d71
oogz <yugenoid@gmail.com>
2017-01-31 22:29:30 -0800

    Added about page

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,11 @@ Rails.application.routes.draw do
         post 'downvote'
       end
     end
+    get "/pages/:page" => "pages#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
